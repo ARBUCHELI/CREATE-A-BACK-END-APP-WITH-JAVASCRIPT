8413ec2000bc2bd4f573fe9e9b1d7cf84378dec1
ARBUCHELI <radio_seattle@hotmail.com>
2024-04-16 08:57:13 +0300

    building back-end services with express.js - introduction to node.js - running a program with node

---
+++ 5. BUILDING BACK-END SERVICES WITH EXPRESS.JS/INTRODUCTION TO NODE.JS/running-a-program-with-node/app.js
@@ -0,0 +1,8 @@
+let noun1 = 'man who walked on the moon';
+let adjective = 'loyal and naive';
+let noun2 = 'hillbilly'
+let verb = 'watch and swallow the lies of the u.s government on tv';
+let noun3 = 'chips and beer';
+
+
+console.log(`The world's first ${noun1} was a very ${adjective} ${noun2} who loved to ${verb} while eating ${noun3} for every meal.`);
